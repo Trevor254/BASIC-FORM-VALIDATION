385da2350c407902dc9f713cb6da3eba5110ce1d
Trevor254 <trevoromondi811@gmail.com>
2019-10-15 16:19:45 +0300

    add regular expression patterns

--- validation.js
+++ validation.js
@@ -6,8 +6,10 @@ const inputs = document.querySelector('input');
 
 const patterns = {
        telephone:/^\d[11]$/,
-        username:/^[a-z\d][5,12]$/i,
-        password:/^[w@-][8,20]$/
+        username:/^[a-z\d]{5,12}$/i,
+        password:/^[w@-]{8,20}$/,
+        slug:/^[a-z\d-]{8,20}$/,
+        email:/^([a-z\d]+)@([a-z\d]+)\.([a-z]{2,8})(\.[a-z])$/
 }
 //validate function
 function validate(field,regex){
